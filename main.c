66c7cc8a32edb03b3d016d2752a4acc39d765661
gilmaramg66 <gialmuno@espol.edu.ec>
2024-12-19 19:36:06 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Taller3ps
+//
+//  Created by Gilmar Munoz on 18/12/24.
+//
+
+#include <stdio.h>
+#include "menu.h"
+
+int main(int argc, const char * argv[]) {
+    showMenu();
+}
